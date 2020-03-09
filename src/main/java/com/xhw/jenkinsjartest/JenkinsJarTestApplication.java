dc0b03745a5f49397c05aa1114ffa1b9d228c6ab
xianghongwu <1074066227@qq.com>
2020-03-09 17:02:32 +0800

    第一次提交

---
+++ src/main/java/com/xhw/jenkinsjartest/JenkinsJarTestApplication.java
@@ -0,0 +1,13 @@
+package com.xhw.jenkinsjartest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsJarTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsJarTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xhw/jenkinsjartest/JenkinsJarTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhw.jenkinsjartest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsJarTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
